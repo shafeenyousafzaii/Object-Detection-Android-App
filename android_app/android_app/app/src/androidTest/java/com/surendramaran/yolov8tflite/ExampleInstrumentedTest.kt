afe849b029320ee11d04bb4cb49fa932cc2e9a00
Muhammad Shafeen <112253449+shafeenyousafzaii@users.noreply.github.com>
2024-07-24 10:30:13 +0500

    Custom Chagnes for self

--- android_app/android_app/app/src/androidTest/java/com/surendramaran/yolov8tflite/ExampleInstrumentedTest.kt
+++ android_app/android_app/app/src/androidTest/java/com/surendramaran/yolov8tflite/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.surendramaran.yolov8tflite", appContext.packageName)
+        assertEquals("com.surendramaran.yolov8n_float32.tflite", appContext.packageName)
     }
 }
