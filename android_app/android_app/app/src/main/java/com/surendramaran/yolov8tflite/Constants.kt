f47eb658579258851843e80f912eba3cf792d2af
Muhammad Shafeen <112253449+shafeenyousafzaii@users.noreply.github.com>
2024-07-24 15:18:32 +0500

    Yolo8n+Yolo8s
    We needed to come up with an object detection model , yolo happens to be the best option . Much modification are on the way
    
    Co-Authored-By: Zeeshan Khalid <109208035+zeeshankhalid2k01@users.noreply.github.com>

--- android_app/android_app/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
+++ android_app/android_app/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -1,6 +1,6 @@
 package com.surendramaran.yolov8tflite
 
 object Constants {
-    const val MODEL_PATH = "yolov8n_float32.tflite"
+    const val MODEL_PATH = "yolov8s_float32.tflite"
     const val LABELS_PATH = "labels.txt"
 }
